3c198c51323443f96d6a52d84417410b1e82b6d2
Ryan Hartzfeld <ryan.hartzfeld@gmail.com>
2014-01-04 16:41:27 -0500

    Numerous changes (see commit details)
    Now allowing users to specify whether they want to animate the display
    of the golden elements or not.  Broke a few of the pieces of
    repetitive/complex logic into function calls.  Other small code tweaks.

--- staygolden.js
+++ staygolden.js
@@ -1,6 +1,104 @@
-var goldenRatio = 1.618;
+var stayGolden = {
+	goldenRatio : 1.618,
+	performCalculations : function(goldenElems, options) {
+		//show the parent
+		var parent = goldenElems.parent();
+		parent.show();
+		
+		/* 
+		 * In this pattern, each pair of divs (e.g. 0 & 1, 2 & 3, 4 & 5, etc...)
+		 * needs to be floated in the same direction, which alternates pair-by-pair.
+		 * This is why you see the extra variables outside of the for loop.
+		 */
+		var floatCounter = 0;
+		var leftNotRight = true;
+		for (var i = 0; i < goldenElems.length; i++) {
 
-/* Protect the jQuery '$' alias by placing plugin code inside an immediately-invoked function expression */
+			var $elem = $(goldenElems[i]);
+
+			// calculate the desired width for this element.
+			var lastWidth;
+			if (i === 0) {
+				//start a little low to prevent any overflowing that occurs because of rounding
+				lastWidth = 99.99;
+			} else {
+				lastWidth = $(goldenElems[i-1]).data('golden-width');
+			}
+			var newWidth = lastWidth / stayGolden.goldenRatio;
+			var roundedWidth = Math.round(newWidth * 1000)/1000;
+
+			//calculate the css float property's value
+			var floatValue;
+			if (leftNotRight) {
+				floatValue = 'left';
+			} else {
+				floatValue = 'right';
+			}
+
+			// set the new CSS percentage width, 
+			// set the float appropriately
+			// and store the (unrounded) new width in this object's data
+			$elem
+				.css('width', roundedWidth + '%')
+				.css('float', floatValue)
+				.data('golden-width', newWidth);
+
+			// square off the element by making its height equal to its rendered width
+			// note that this value is adjusted whenever the window is resized
+			// also hide before the user can see the rendered result
+			$elem.makeSquare().hide();
+
+			if (options.animate === false) {
+				stayGolden.showElement($elem, options);
+			} else {
+				// use immediately-invoked function expression to ensure that the proper element is referenced since we
+				// are performing an asynchronous action inside of a repetition structure. After a short delay, trigger
+				// a fadeIn effect on each subsequent element and call our callback if necessary
+				(function(elemToShow, counter) {
+					var delay = counter * options.speed + 1;
+					setTimeout(
+						function() {
+							stayGolden.showElement(elemToShow, options);
+						}, delay
+					);
+				})($elem, i);
+			}
+			
+			// increment our float counter and make necessary adjustments for the next loop iteration
+			floatCounter++;
+			if (floatCounter === 2) {
+				//flip the float, yo
+				leftNotRight = !leftNotRight;
+				floatCounter = 0;
+			}
+		}
+
+		return this;
+	},
+	showElement: function(elemToShow, options) {
+		if (options.eachDone !== null && typeof options.eachDone === 'function') {
+			if (options.animate === false) {
+				//don't animate, use function.prototype.apply() to apply scope
+				elemToShow.show();
+				options.eachDone.apply(elemToShow);
+			} else {
+				//animate, use jQuery fadeIn's built-in callback handler
+				elemToShow.fadeIn(options.speed, options.eachDone);
+			}
+		} else {
+			if (options.animate === false) {
+				//don't animate
+				elemToShow.show();
+			} else {
+				//animate
+				elemToShow.fadeIn(options.speed);
+			}
+		}
+	}
+};
+
+/* jQuery plugins
+ * Protect the jQuery '$' alias by placing plugin code inside an immediately-invoked function expression */
 (function ($) {
 	
 	// nifty golden ratio jQuery plugin
@@ -10,7 +108,7 @@ var goldenRatio = 1.618;
 		var _this = this;
 
 		//hide the parent div until just before we are ready to go
-		var parent = _this.parent().hide();
+		_this.parent().hide();
 
 
 		//build options object by merging default values with passed arguments
@@ -19,6 +117,8 @@ var goldenRatio = 1.618;
 		var options = $.extend({
 			delay : 1,
 			speed : 500,
+			animate: true,
+			eachDone: null
 		}, args);
 
 		//convert acceptable string delays into millisecond delays
@@ -51,83 +151,13 @@ var goldenRatio = 1.618;
 			}
 		}
 
-		setTimeout(function() {
-			//show the parent
-			parent.show();
-			
-			/* 
-			 * In this pattern, each pair of divs (e.g. 0 & 1, 2 & 3, 4 & 5, etc...)
-			 * needs to be floated in the same direction, which alternates pair-by-pair.
-			 * This is why you see the extra variables outside of the for loop.
-			 */
-			var floatCounter = 0;
-			var leftNotRight = true;
-			for (var i = 0; i < _this.length; i++) {
-
-				var $elem = $(_this[i]);
-
-				// calculate the desired width for this element.
-				var lastWidth;
-				if (i === 0) {
-					//start a little low to prevent any overflowing that occurs because of rounding
-					lastWidth = 99.99;
-				} else {
-					lastWidth = $(_this[i-1]).data('golden-width');
-				}
-				var newWidth = lastWidth / goldenRatio;
-				var roundedWidth = Math.round(newWidth * 1000)/1000;
-
-				//calculate the css float property's value
-				var floatValue;
-				if (leftNotRight) {
-					floatValue = 'left';
-				} else {
-					floatValue = 'right';
-				}
-
-				// set the new CSS percentage width, 
-				// set the float appropriately
-				// and store the (unrounded) new width in this object's data
-				$elem
-					.css('width', roundedWidth + '%')
-					.css('float', floatValue)
-					.data('golden-width', newWidth);
-
-				// square off the element by making its height equal to its rendered width
-				// note that this value is adjusted whenever the window is resized
-				// also hide before the user can see the rendered result
-				$elem.makeSquare().hide();
-
-				// use immediately-invoked function expression to ensure that the proper element is referenced since we
-				// are performing an asynchronous action inside of a repetition structure. After a short delay, trigger
-				// a fadeIn effect on each subsequent element and call our callback if necessary
-				(function(elemToShow, counter) {
-					var delay = counter * options.speed + 1;
-					setTimeout(
-						function() {
-							if (args.hasOwnProperty('eachDone') && typeof args.eachDone === 'function') {
-								elemToShow.fadeIn(options.speed, args.eachDone);
-							} else {
-								elemToShow.fadeIn(options.speed);
-							}
-							
-						}, delay
-					);
-				})($elem, i);
-				
-				
-
-				// increment our float counter and make necessary adjustments for the next loop iteration
-				floatCounter++;
-				if (floatCounter === 2) {
-					//flip the float, yo
-					leftNotRight = !leftNotRight;
-					floatCounter = 0;
-				}
-			}
-
-			return this;
-		}, options.delay);
+		if (options.animate === false) {
+			stayGolden.performCalculations(_this, options);
+		} else {
+			setTimeout(function() {
+				stayGolden.performCalculations(_this, options);
+			}, options.delay);
+		}
 	};
 
 	$.fn.fitToWindow = function() {
